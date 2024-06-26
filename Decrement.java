e9bdcd4342a682dcd6b42e1a55939f555bfb0947
Vaishnavi2024-B <vaishnavi123.xworkz@gmail.com>
2024-06-27 00:00:40 +0530

    Task

---
+++ Decrement.java
@@ -0,0 +1,11 @@
+public class Decrement {
+    public static void main(String[] args) {
+        int x = 87; 
+        int value;
+		int value1 = x--;
+		int value2 = --x;
+		System.out.println("Post Increment Operation:" +value1);
+        System.out.println("Pre Increment Operation:"+value2 );
+        
+    }
+}
